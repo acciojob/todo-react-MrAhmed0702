3348c9ed9999153c484e053a83207e9e785ab1e0
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-11-01 02:08:36 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Input from "./Input";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (text) => {
+    setTodos([...todos, text]);
+  }
+
+  function handleDelete(index){
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <Input onAdd={handleAddTodo} />
+        <ul>
+            {todos.map((todo, index) => (
+                <li key={index}>
+                    {todo}
+                    <button onClick={() => handleDelete(index)}>Delete</button>
+                </li>
+            ))}
+        </ul>
     </div>
   )
 }
